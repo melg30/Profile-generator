75710eedbb43289827d63b6c8976a09ee25ee123
Mel Garza <melgarza3016@gmail.com>
2020-01-12 10:05:13 -0700

    Added starter code

---
+++ starter-files-for-Node-HW/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+// Require Inquirer
+const inquirer = require("inquirer");
+// Require Axios
+const axios = require("axios");
+// Require HTML/PDF
+const pdf = require("html-pdf");
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
