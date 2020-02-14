6c508464e5acb70eefdba3210328bea04bc9c6d4
Mel Garza <melgarza3016@gmail.com>
2020-02-14 15:36:31 -0600

    added package.json and requires

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const questions = [];
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const pdf = require("html-pdf");
+const generateHTML = require("./generateHTML");
+
+const questions = [
+  {
+    type: "input",
+    message: "Enter your GitHub username",
+    name: "username"
+  },
+  {
+    type: "list",
+    message: "What is your favorite color?",
+    choices: [
+      "red",
+      "orange",
+      "yellow",
+      "green",
+      "blue",
+      "purple",
+      "pink",
+      "hotpink",
+      "teal"
+    ],
+    name: "favorite_color"
+  }
+];
 
 function writeToFile(fileName, data) {}
 
