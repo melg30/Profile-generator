eaf2c74607299c3899ce6a63bbeffdd833a4f98c
Mel Garza <melgarza3016@gmail.com>
2020-02-14 16:09:59 -0600

    updated index.html

--- index.js
+++ index.js
@@ -4,29 +4,32 @@ const inquirer = require("inquirer");
 const pdf = require("html-pdf");
 const generateHTML = require("./generateHTML");
 
-const questions = [
-  {
-    type: "input",
-    message: "Enter your GitHub username",
-    name: "username"
-  },
-  {
-    type: "list",
-    message: "What is your favorite color?",
-    choices: [
-      "red",
-      "orange",
-      "yellow",
-      "green",
-      "blue",
-      "purple",
-      "pink",
-      "hotpink",
-      "teal"
-    ],
-    name: "favorite_color"
-  }
-];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username"
+    },
+    {
+      type: "list",
+      message: "What is your favorite color?",
+      choices: ["green", "blue", "pink", "red"],
+      name: "color"
+    }
+  ])
+  .then(response => {
+    //Define the Username & Color
+    usernameInput = response.username;
+    console.log(usernameInput);
+
+    colorInput = response.color;
+    console.log(colorInput);
+
+    //Call functions
+    stars(usernameInput);
+    gitHub(usernameInput);
+  });
 
 function writeToFile(fileName, data) {}
 
